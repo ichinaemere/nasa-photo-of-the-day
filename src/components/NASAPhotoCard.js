e23d95f2b84a3cce7fda4f611c5c155318ca729b
ichinaemere <ms.chinaemere@gmail.com>
2019-10-24 19:01:56 +0000

    99% MVP. Everything is displaying but my Image and I'm not sure why.

--- src/components/NASAPhotoCard.js
+++ src/components/NASAPhotoCard.js
@@ -1,11 +1,19 @@
 import React from "react";
 
-export default function NASAPhotoCard(){
+const NASAPhotoCard = props => {
 
 
 
 
-    // return(
+    return(
+        <div>
+            <h1>NASA Photo of The Day</h1>
+            <h2>Title: {props.title} </h2>
+            <h2>Date: {props.date} </h2>
+            <img src={props.url}/>
+            <p>Explanation: {props.explanation}</p>
+        </div>
+    );
+};
 
-    // )
-}
+export default NASAPhotoCard;
--- src/components/NASAPhotoInfo.js
+++ src/components/NASAPhotoInfo.js
@@ -1,20 +1,28 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import NASAPhotoCard from "./NASAPhotoCard.js";
 
 export default function NASAPhotoInfo(){
 
-    const [photoData, setPhotoData] = useState({});
+    const [photoData, setPhotoData] = useState([]);
 
     useEffect(()=> {
-        axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+        axios.get(`https://api.nasa.gov/planetary/apod?api_key=DiV6Dc0hJ58lBFPqxDtaJ3SFBbNZGcZRIKJDtp5h`)
         .then(response => {
-            console.log(response);
+            console.log(response.data);
+            setPhotoData(response.data);
         })
         .catch(error => {
             console.log("the data was not returned", error)
         })
-    })
+    }, []);
+
     return(
-        <div></div>
-    )
+        <div>
+
+         <NASAPhotoCard title={photoData.title}
+            date={photoData.date} explanation={photoData.explanation} imgURL={photoData.url}/>
+        </div>
+
+    );
 }
